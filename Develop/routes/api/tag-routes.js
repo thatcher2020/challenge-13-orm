c507d0523ac27b23b89616d15f56e819cdd31d45
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-07 15:56:43 -0400

    some end points for tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  // be sure to include its associated Product da
+router.get('/', (req, res) => {
   try {
     const tags = await Tag.findAll({
       include: 
@@ -17,22 +16,39 @@ router.get('/', (req, res) => {
   }
 });
 
-
-router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+router.get('/:id', (req, res) => {
+  try {
+    const tag = await Tag.findByPk = await Tag.findByPk(req.params.id, {
+      include: 
+      [{ model: Product, through: ProductTag, as: 'tagged_products' }],
+    });
+
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
   // create a new tag
+router.post('/', (req, res) => {
+
 });
 
-router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+router.put('/:id', (req, res) => {
+
 });
 
-router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+router.delete('/:id', (req, res) => {
+
 });
 
 module.exports = router;
