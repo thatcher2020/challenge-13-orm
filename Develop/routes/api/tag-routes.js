66f7193519be5f93264a4589e1bf6e7d8f8a343b
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-07 15:54:37 -0400

    some end points for tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,7 +6,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: 
+      [{ model: Product, through: ProductTag, as: 'tagged_products' }],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
