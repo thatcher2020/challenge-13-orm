a094a50319c208483c7c955134dde91904312f35
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-13 20:39:33 -0400

    changes to price

--- Develop/seeds/product-seeds.js
+++ Develop/seeds/product-seeds.js
@@ -2,33 +2,33 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
+    product_name: 'T-Shirt',
+    price: 20.00,
+    stock: 20,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
-    price: 90.0,
-    stock: 25,
+    product_name: 'Shoes',
+    price: 100.00,
+    stock: 20,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
-    price: 22.99,
-    stock: 12,
+    product_name: 'Baseball Hat',
+    price: 20.00,
+    stock: 20,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
-    stock: 50,
+    product_name: 'CD Player',
+    price: 10.00,
+    stock: 20,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
-    stock: 22,
+    product_name: 'Shorts',
+    price: 25.00,
+    stock: 20,
     category_id: 2,
   },
 ];
